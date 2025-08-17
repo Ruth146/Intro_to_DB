448745163f0d2fb71222cf6e71e7257f19f3a856
Ruth146 <Ruth.abfanossie@gmail.com>
2025-08-17 21:14:47 +0300

    Create task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- TASK_5.SQL
+-- INSERT A SINGLE ROW INTO Customers TABLE
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
